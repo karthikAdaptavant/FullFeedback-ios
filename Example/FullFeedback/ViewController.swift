1ca3d384e587582a878e2cf68ae9fd0d41e6ea01
Karthik <karthik.samy@a-cti.com>
2019-06-25 19:58:39 +0530

    cleanups

--- Example/FullFeedback/ViewController.swift
+++ Example/FullFeedback/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
         
         let feedBackSign = "<br><br>----------<b>App Data</b>-----------<br> <br>------------<b>User Info</b>----------<br> <b>LoginId</b> - sathish.gurunathan@anywhere.co<br> <b>UserName</b> - sathish<br> <b>UniquePin</b> - Test<br> <b>TimeZone</b> - In/kolkata - 05:30<br> <br>------------<b>App Info</b>----------<br> <b>AppVersion</b> - v1.0(1.3)<br> <b>AppEnvironment</b> - TestLive<br> <b>Brand ID</b> - TestBrandID<br> <br>------------<b>Device Info</b>----------<br> <b>Model</b> - iPhone<br> <b>DeviceType</b> - Test<br> <b>Version</b> - iOS 11.1.1<br> <b>DeviceName</b> - iPhone<br> <b>Device ID</b> - TestID<br>"
         
-        let dsAwParmaHelper = DSParamHelper(department: "85bc57f0-8628-41f3-82e5-42fc0836883c", departmentId: "486a0382-b82c-4c4c-9e15-4051f58eeaba", brandId: "0db91e28-1e09-4420-9390-faac4ab2c931", type: "ae3dd607-c06c-4ecd-8c2a-27507e7e77dd", source: "ios feedback", accessToken: accessToken, emailId: "sathish.gurunathan@anywhere.co")
+        let dsAwParmaHelper = DSParamHelper(department: "85bc57f0-8628-41f3-82e5-42fc0836883c", departmentId: "486a0382-b82c-4c4c-9e15-4051f58eeaba", type: "0db91e28-1e09-4420-9390-faac4ab2c931", source: "ae3dd607-c06c-4ecd-8c2a-27507e7e77dd", accessToken: "ios feedback", emailId: accessToken, brandId: "sathish.gurunathan@anywhere.co")
         
         
         guard let feedbackVc = FeedbackViewController.initialize(dsParamHelper: dsAwParmaHelper, appType: .awFeedback, feedbackInfo: feedBackSign) else {
